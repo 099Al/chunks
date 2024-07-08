07d1a03961a403e95243c74cfcec8a0fe71181c0
099Al <alexef157@gmail.com>
2024-07-09 00:34:49 +0500

    format

--- chunks_v1.py
+++ chunks_v1.py
@@ -2,8 +2,11 @@ import pandas as pd
 
 import numpy as np
 
-def make_timeseries(start="2000-01-01", end="2000-12-31", freq="1D", seed=None):
-    index = pd.date_range(start=start, end=end, freq=freq, name="timestamp")
+
+def make_timeseries(
+    start="2023-01-01 00:00:00", end="2023-01-01 00:00:15", freq="1s", seed=None
+):
+    index = pd.date_range(start=start, end=end, freq=freq, name="dt")
     n = len(index)
     state = np.random.RandomState(seed)
     columns = {
@@ -16,39 +19,42 @@ def make_timeseries(start="2000-01-01", end="2000-12-31", freq="1D", seed=None):
         df = df.iloc[:-1]
     return df
 
+
 def prepare_data():
     timeseries = [
         make_timeseries(freq="1min", seed=i).rename(columns=lambda x: f"{x}_{i}")
         for i in range(10)
     ]
     ts_wide = pd.concat(timeseries, axis=1)
-    ts_wide.to_csv('csv_chunk.csv', index=False)
-
+    ts_wide.to_csv("csv_chunk.csv", index=False)
 
 
 def test_asset():
 
-    dfs = pd.date_range(
-        "2023-01-01 00:00:00",
-        "2023-01-01 00:00:05",
-        freq="s"
-    )
+    dfs = pd.date_range("2023-01-01 00:00:00", "2023-01-01 00:00:05", freq="s")
     df = pd.DataFrame({"dt": dfs.repeat(3)})
 
     return df
+
 
 """
 Желаемый размер чанка определяется значением b
 """
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+
+    df = make_timeseries(
+        start="2023-01-01 00:00:00", end="2023-01-01 00:00:15", freq="1s", seed=None
+    )
+    df.to_csv("text_6.csv")
+
+    exit()
 
     df = test_asset()
     # df = pd.read_csv('csv_chunk.csv')
 
-
     row_cnt, col_cnt = df.shape
-    print('rows',row_cnt)
+    print("rows", row_cnt)
 
     a, b = 1, 7
 
@@ -56,29 +62,28 @@ if __name__ == '__main__':
     s = 0
     cnt = 0
     l = []
-    for x in range(0,row_cnt):
+    for x in range(0, row_cnt):
 
         if x == 6:
             pass
         row = df.iloc[x]
-        v = row['dt']
+        v = row["dt"]
         if v == prev_dt:
             cnt += 1
         else:
-            if (cnt < b):
+            if cnt < b:
                 cnt += 1
             else:
-                l.append((s, x-1))
+                l.append((s, x - 1))
                 s = x
                 cnt = 1
         prev_dt = v
 
-    if row_cnt-s > 0:
+    if row_cnt - s > 0:
         l.append((s, row_cnt))
 
     print(l)
 
-    for a,b in l:
-        print(f'{a}-{b}----------------------')
-        print(df.iloc[a:b+1])
-
+    for a, b in l:
+        print(f"{a}-{b}----------------------")
+        print(df.iloc[a : b + 1])
